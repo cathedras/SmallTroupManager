cd92b17cf0a8fd9f8bf93e251433af681d695647
cathedra <940747003@qq.com>
2019-12-05 17:56:38 +0800

    增加文件 +Gbl +Resources/ListColItemResource.xaml +Resources/ListColTemp.cs

---
+++ SmallTroupManager/Resources/ListColTemp.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using SmallTroupManager.Model;
+namespace SmallTroupManager.Resources
+{
+    public class ListColTemp : DataTemplateSelector
+    {
+        public DataTemplate EditTemplate { get; set; }
+        public DataTemplate ShowTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (((RepertoireItem)item).CurState == State.Edit)
+            {
+                return EditTemplate;
+            }
+            else if(((RepertoireItem)item).CurState == State.Show)
+            {
+                return ShowTemplate;
+            }
+            else
+            {
+                return base.SelectTemplate(item, container);
+            }
+           
+        }
+    }
+}
---
+++ SmallTroupManager/Utils/Gbl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElCommon.Util;
+namespace SmallTroupManager.Utils
+{
+    public class Gbl:IntrinsicCfg
+    {
+
+    }
+}
